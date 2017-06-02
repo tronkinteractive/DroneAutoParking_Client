e1cca5892f7a797a4df95c3e73b190784b1f65f9
weichih25tronk <weichih25@tronk.tw>
2017-06-02 22:48:28 +0800

    This client program can send text message to the server.

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxNetwork.h"
 
 class ofApp : public ofBaseApp{
 
@@ -21,4 +22,16 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+		ofxTCPClient tcpClient;
+		string msgTx, msgRx;
+
+		ofTrueTypeFont  mono;
+		ofTrueTypeFont  monosm;
+
+		float counter;
+		int connectTime;
+		int deltaTime;
+
+		int size;
+
 };
